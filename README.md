5f614e6377759643a2a44b8f398d76403fe579fb
chollingsworth <caleb.hollingsworth10@gmail.com>
2022-07-17 11:21:38 -0500

    intial commit

--- README.md
+++ README.md
@@ -5,37 +5,30 @@ Uses google translate througe googletrans npm package. It doesn't require a key,
 
 # Requirements
 In order to use this script you must
-* Create a Twitch channel for your bot. The name of th ebot will be the name of the channel and cannot be changed
+* Create a Twitch channel for your bot. The name of the bot will be the name of the channel and cannot be changed
 * Generate a key for authenticating your bot, using the Twitch Chat OAuth Password Generator: https://twitchapps.com/tmi/
 
 # Setup
-You only need to install npm packages and run the script, by executing these commands in the folder where you have stored your copy of `bot.js` script. You'll need to set CHANNEL_NAME and CHANNEL_PASSWORD environment variables to node - you also can directly modify the script
+You only need to install npm packages and run the script, by executing these commands in the folder where you have stored your copy of `bot.js` script. You'll need to set CHANNEL_NAME and CHANNEL_PASSWORD environment variables to node.
 
 ```
 npm install
-CHANNEL_NAME=mybotchannel CHANNEL_PASSWORD=xxxxxx node ./bot.js
+BOT_CHANNEL_NAME=yourBotName BOT_CHANNEL_PASSWORD=yourBotKey
+POST_CHANNEL_NAME=yourChannelName
+node ./bot.js
 ```
 
-# Commands
+To stop the bot, you only need to press:
 
-- !lang : displays basic information about the feature.
+```
+ctrl+C
+```
 
-Language related commands:
+# Updating Changes
 
-- `!cn` : Translate to Simplified Chinese    
-- `!en` : Translate to English
-- `!fi` : Translate to Finnish    
-- `!fr` : Translate to French
-- `!de` or `!ge` : Translate to German    
-- `!it` : Translate to Italian
-- `!jp` : Translate to Japanese
-- `!ko` : Translate to Korean
-- `!pl` : Translate to Polish
-- `!pt` or `!br` : Translate to Portuguese
-- `!ro` : Translate to Romanian   
-- `!tu` : Translate to Turkish
-- `!ru` : Translate to Russian
+After you have cloned this repository, you can pull updates to the code by going into the terminal (or git terminal), navigating to the location of this repository on your local machine (make sure your bot isn't currently running), and typing:
 
-For example: `!pt Hello friends!`
-
-Emoticons should be avoided as they will disturb the bot (and it won't probably be able to display them). Also  mixing different languages in your sentence shoudl be avoid.
+```
+git pull origin master
+```
+This will pull updated code, and at that point.
--- bot.js
+++ bot.js
@@ -1,10 +1,122 @@
-// Translator Bot for Twitch
-// Using google translate, via 'googletrans' package
-
-// API Twitch
+// Kine's Translator Bot for Twitch
+require('dotenv').config();
 const tmi = require('tmi.js');
-// API Google Translate
 const gtrans = require('googletrans').default;
+const ignoreUsers = ['Nightbot', 'BikuBikuTest']
+const ignoreWords = [
+  'http', 'thekineDumpy', 'nt', 'ntnt', '<3',
+  'Kappa', 'thekineLove', 'thekineBubbletoss',
+  'Kreygasm', 'thekineScarz', 'thekineCry',
+  'thekineStuck', 'doopleGrip', 'doopleLove', 'LUL',
+  'VoteYea', 'VoteNay', ':)', 'NotLikeThis', 'TheIlluminati',
+  'HeyGuys', 'PogChamp', 'VoHiYo', 'BibleThump', 'WutFace',
+  'ResidentSleeper', 'thekineDeded', 'thekineDumpy', 'thekineJam',
+  'thekineWiggle', 'thekineBubbletoss', 'thekineREEE', 'thekineLove',
+  'thekineBubble', 'thekineYikes', 'thekineSip', 'thekinePeek',
+  'thekinePat', 'thekineNotes', 'thekineNom', 'thekineSleep',
+  'thekineKiss', 'thekineHug', 'thekineHmm', 'thekineFlex',
+  'thekineBigfan', 'thekineDerp', 'thekineDeaf', 'thekinePork',
+  'thekineDed', 'thekineCry', 'thekineShy', 'thekineBless',
+  'thekineAngy', 'thekineGgs', 'thekineRage', 'thekineSmart',
+  'thekineWingman', 'thekinePg', 'thekinePork', 'thekineFace',
+  'thekineWtf', 'thekineScarz', 'thekineStuck', 'VeryPog',
+  'TriSad', 'sumSmash', 'dinkDonk', 'Smile', 'Thinking2',
+  'Thinking', 'ppSmoke', 'Binoculars', 'NoThanks', 'BASED',
+  '4House', 'Weirdge', 'Handsup', 'GoodMeme', 'BUSSERS',
+  'SHUSHERS', 'HYPERNODDERS', 'HYPERNOPERS', 'FLAPPERS',
+  'BLUBBERS', 'GoodTake', 'Applecatrun', 'DANKIES', 'catKISS',
+  'Drake', 'OMEGALULiguess', 'Jammies', 'SALAMI', 'pepeMeltdown',
+  'peepoChat', 'PEEPEES', 'peepoRain', 'TriDance', 'BLANKIES',
+  'peepoSleep', 'Madge', 'donowall', 'donkGiggles', 'HYPERCLAP',
+  'peepoCoffee', 'ppOverheat', 'gachiPRIDE', 'Slap', 'MONKE',
+  'GoodMorning', 'Gladgers', 'NOOOO', 'DankG', 'Tastyge',
+  'CoffeeTime', 'MEGA', 'MONKE', 'Bedge', 'FeelsLagMan',
+  'DonkCredit', 'GIGACHAD', 'BOOBA', 'monkaTOS', 'Scoots',
+  'whycantthisfuckingbemegoddamnit', 'Prayge', 'ModTime',
+  'PETTHEPEEPO', 'PepegaCard', 'WICKED', 'PETTHESTREAMER',
+  'peepoShortOnAGoose', 'KEKWait', 'popCat', 'Modge',
+  'COCKA', 'pugPls', 'SaxTime', 'POGGIES', 'PizzaTime',
+  'DonoTime', 'pepePHONE', 'YEP', 'Offline', 'Smoge',
+  'ApuChef', 'COGGERS', 'PETTHEVIPS', 'ACTINUP',
+  'monkaHmm', 'peepoTalk', 'POGGIESS', 'ppSkate',
+  'PepoPopcorn', 'peepoJail', 'KKonaLand', 'KEKL',
+  'Saved', 'BANGER', 'peepoLegs', 'waterTime', 'Clueless',
+  'dittoDumper', 'glizzyL', 'glizzyR', 'Applecatgun',
+  'dogJAM', 'peepoFlower', 'Oldge', 'MODSS', 'BOOMIES',
+  'Homi', 'CatAHomie', 'MonkeKiss', 'JointTime', 'FeelsDabMan',
+  'widepeepoHug', 'peepoRoo', 'peepoFoil', 'SURE', 'peepoCheer',
+  'Kissaweeb', 'xqcMald', 'WAYTOODANK', 'PeepoFinger', 'peepoStare',
+  'YEPCOCK', 'peepoWagon', 'PepeSpit', 'peepoBlanketShare', 'DankHandshake',
+  'DankVrum', 'stopbeingMean', 'LETSGOOO', 'SCAMMED', 'monkaHide',
+  'PepegaAim', 'pepeBREAKDANCE', 'donkWalk', 'Radge', 'SUSSY', 'LipBite',
+  'YEO', 'peepoFood', 'lebronJAM', 'monkeySip', 'Bananacatrun', 'PogU',
+  'FeelsWeakMan', 'GIGADONK', 'peepoGamble', 'ChefsKiss', 'peepoLove',
+  'Businessge', 'GoldenKappa', 'KEKWTF', 'Nerdge', 'peepoSit',
+  'pepePoint', 'PlsBanMe', 'APEXFROG', 'PepeMoney', 'peeTime',
+  'CopiumTime', 'FeelsDankVip', 'peepoComfy', 'peepoPooPoo',
+  'peepoBand', 'Slapahomie', 'PogFish', 'PogOGrab', 'Kissahomie',
+  'OME', 'VIPS', 'Chatting', 'PepeLoser', 'peepoRiot', 'peepoBlush',
+  'YesYes', 'bruh', 'ewApex', 'DIESOFCRINGE', 'peepoEvil',
+  'DonkSass', 'BOTHA', 'Wokege', 'peepoHappyRoll', 'peepoMarch',
+  'catAwkward', 'applecatPanik', 'Avocatospin',
+  'FullEpisodeOfRickAndMortySeason3Episode3ButSpedUpSuperFastImPickleRickMortyITurnedMyselfIntoAPickle',
+  'WICKEDcock', 'XiPls', 'PepegaSlide', 'mandeSucccccc',
+  'SALAMIhand', 'orangeCatRun', 'Adge',
+  'POUND', 'TakingNotesL', 'HomieKiss', 'MODS',
+  '2TIME', 'Aware', 'HUH', 'BowTime', '3Pepekeklul',
+  'TinaTwerk', 'gettingSturdy', 'peepoCappin', 'ElmoDance',
+  'Pumpkincatrun', 'BINGCHILLING', 'PikaYeet', 'ThisIsFine',
+  'Sip', 'ICANT', 'GAMBAADDICT', 'xqcCheer', 'WhoAsked',
+  'WeirdManDude', '4Weird', 'CatChest', 'ppZip', 'ApexServers',
+  'applecatCHASE', 'MathTime', 'AYO', 'peepoJuiceSpin',
+  'RainbowTwerk', 'GROUPA', 'WigglesAround', 'peepoSub',
+  'md7GroupJAM', 'Vibe', 'Binoculous', 'GroupWankge',
+  'willSlap', 'PeepoSZpride', 'Batting', 'xdd',
+  'peepoPrime', 'WOT', 'iitzTake', 'JOHNNYCANT',
+  'GIbbyTat', 'ClutchPlays', 'LegalHacks', 'SleepyTime',
+  'wiggExit', 'Hearding', 'Copege', 'ChairStream', 'gachiBASS',
+  'peepoHappy', 'BillyApprove', 'WAYTOODANK', 'WineTime',
+  'FeelsDankMan', 'RebeccaBlack', 'FeelsWeirdMan', '7tvM',
+  'reckH', 'FeelsOkayMan', 'CrayonTime', 'BasedGod',
+  'forsenPls', 'TeaTime', 'Clap', 'GuitarTime', 'Clap2',
+  'FeelsStrongMan', 'gachiGASM', 'nymnCorn', 'RareParrot',
+  'AYAYA', 'knaDyppaHopeep', 'PianoTime', 'peepoSad',
+  'YEAHBUT7TV', 'PepePls', 'AlienDance', 'EZ', 'ppL',
+  'Stare', 'RainTime', 'PETPET', 'SteerR', 'PartyParrot',
+  'ApuApustaja', 'Gayge', 'EZ', 'peepoSmash', 'MLADY',
+  'sumSmash', 'KKool', 'FeelsLateMan', 'GuitarTime',
+  'FeelsLoveMan', 'widepeepoHeart', 'gachiHYPER',
+  'PepoSabers', 'pepeClap', 'kmdWeee', 'peepoRain',
+  'Hmmm', 'BLELELE', 'MmmShake', 'TriHardoM',
+  'peepoNaruSprint', 'peepoStrongest', 'PepegaGun', 'PepegaAim',
+  'PepegaDitch', 'BearHug', 'peepoFatrun', 'peepoExit', 'BongoFat',
+  'WeirdChamping', 'catJAM', 'ModTime', 'Brows', 'pompomD', 'PepegaCredit',
+  'DONTPETTHEPEEPO', 'peepoSHAKE', 'gachiBASS', 'PepoDance', 'pepeD',
+  'CLAP', 'PepoCheer', 'peepoClap', 'SadgeCry', 'peepoShy', 'Jammies',
+  'TriDance', 'TriKool', 'pepeJAM', 'PEPEDS', 'peepoWink', 'peepoRun',
+  'peepoArrive', 'peepoLeave', 'peepoGiggles', 'peepoPats', 'HACKERMANS',
+  'peepoPooPoo', 'peepoBye', 'peepoHey', 'NOPERS', 'NODDERS', 'peepoJAMMER',
+  'peepoWine', 'ppOverheat', 'PogUU', 'PauseChamp', 'iLOVEyou', 'Prayge', 'pugPls',
+  'popCat', 'modCheck', 'DanceBro', 'monkaSTEER', 'pepeL', 'PETTHEMODS', 'HYPERPOGGER',
+  'SmokeTime', 'gibbyJAM', 'COPIUM', 'PepoG', 'dittoDumper', 'peepoVrumvrum', 'PagMan',
+  'PogO', 'BOFA', 'Segz', ':tf:', 'CiGrip', 'DatSauce', 'ForeverAlone', 'GabeN',
+  'HailHelix', 'ShoopDaWhoop', 'M&Mjc', 'bttvNice', 'TwaT', 'WatChuSay', 'tehPoleCat',
+  'AngelThump', 'TaxiBro', 'BroBalt', 'CandianRage', 'D:', 'VisLaud', 'KaRappa',
+  'FishMoley', 'Hhhehehe', 'KKona', 'PoleDoge', 'sosGame', 'CruW', 'RarePepe',
+  'haHAA', 'FeelsBirthdayMan', 'RonSmug', 'KappaCool', 'FeelsBadMan', 'bUrself',
+  'ConcernDoge', 'FeelsGoodMan', 'FireSpeed', 'NaM', 'SourPls', 'LuL', 'SaltyCorn',
+  'monkaS', 'VapeNation', 'ariW', 'notsquishY', 'FeelsAmazingMan', 'DuckerZ',
+  'SqShy', 'Wowee', 'WubTF', 'cvR', 'cvL', 'cvHazmat', 'cvMask', 'DogChamp',
+  'KEKW', 'Pog', '3Head', 'PepeClown', 'Pog', 'widepeepoHappy', 'widepeepoSad',
+  'widepeepoBlanket', 'peepoFinger', 'PepeMods', 'MONKERS', 'LULW', 'KEKW', 'OMEGALUL',
+  'PauseChamp', 'PepeLaugh', 'peepoSad', 'POGGERS', '5Head', 'peepoFat', 'forsenCD',
+  'Okayge', 'peepoSus', '4WeirdW', 'pepePoint', 'FeelsStrongMan', 'HYPERS', 'Kapp',
+  'monkaChrist', 'monkaW', 'NOP', 'Sadge', 'YEP', 'FeelsWowMan', 'peepoHappy',
+  'PepoThink', 'Pepepains', 'POOGERS', 'peepoWTF', 'PeepoWeird', 'Pepega',
+  'PepegaPhone', 'PepeHands', 'WaitWhat', 'widepeepoHigh', 'COPIUM', 'monkaS',
+  'FeelsGoodMan', 'FeelsBadMan', 'sup', 'huh', 'lol', 'Lol', 'LOl', 'LOL', 'lOL', 'loL', 'haha'
+]
+
 
 // Define configuration options here
 // - username is the name of the channel/bot
@@ -13,26 +125,26 @@ const gtrans = require('googletrans').default;
 // - connection: defines additional options
 //
 // username and password are defined after CHANNEL_NAME and CHANNEL_PASSWORD environment variables
-// (you also can overwrite botname and botpassword here)
-let botname = process.env.CHANNEL_NAME;
-let botpassword = process.env.CHANNEL_PASSWORD;
-// TODO: If no channel & password, then exit...
-if ((botname == undefined) || (botpassword == undefined)) {
+// (you also can overwrite botName and botPassword here)
+
+let botName = process.env.CHANNEL_NAME;
+let botPassword = process.env.CHANNEL_PASSWORD;
+
+// If no channel & password, then exit...
+if ((botName == undefined) || (botPassword == undefined)) {
   console.error('No CHANNEL_NAME or CHANNEL_PASSWORD environment variable found. Exiting.');
   process.exit(-1);
 }
-// add oauth: before botpassword
-botpassword = 'oauth:' + botpassword;
+// add oauth: before botPassword
+botPassword = 'oauth:' + botPassword;
 
 const opts = {
   identity: {
-    username: botname,
-    password: botpassword
+    username: botName,
+    password: botPassword
   },
   channels: [
-    // using bot's channel is great for testing, but not mandatory
-    botname
-    //more channels
+    'jan_levinson_i_presume'
   ],
   // Automatic reconnection
   connection: { reconnect: true }
@@ -45,6 +157,7 @@ const tr_lang = {
   'en': ['en', 'says'],
   'fr': ['fr', 'dit'],
   'pt': ['pt', 'disse'],
+  'ja': ['ja', ''],
 };
 
 // Register our event handlers (defined below)
@@ -56,13 +169,22 @@ client.connect();
 
 // Called every time a message comes in
 function onMessageHandler(target, context, msg, self) {
+  // implement variable to change target trans lang
+  if (msg[0] === '!') {
+    return;
+  };
+  const userSending = context?.username;
+  if (ignoreUsers.includes(userSending)) {
+    return;
+  };
+
   try {
 
     // Ignore messages from the bot
     if (self) { return; }
 
     // Remove whitespace from chat message
-    let tMsg = msg.trim();
+    let tMsg = '!ja' + ' ' + msg.trim();
 
     // Check if the message starts with @name
     // in that case, extract the name and move the @name at the end of the message, and process
@@ -73,7 +195,6 @@ function onMessageHandler(target, context, msg, self) {
       tMsg = msg + ' ' + atname;
       console.info('Changed message :', tMsg);
     }
-
     // Filter commands (options)
     if (tMsg[0] != '!') return;
 
@@ -89,22 +210,18 @@ function onMessageHandler(target, context, msg, self) {
       return;
     }
 
-    // Commands for displaying messages explaining the translation feature in various languages
-    // TODO: sentences
-    const explanations = {
-      //    'germans': '',
-      //    'spanish': '',
-      'french': 'Vous pouvez utiliser notre bot traducteur. Commencez votre message par !en pour traduire votre message en anglais. Par exemple "!en Bonjour"',
-    }
-    if (cmd in explanations) {
-      client.say(target, explanations[cmd]);
-      return;
-    }
-
     if (cmd in tr_lang) {
       var ll = tr_lang[cmd];
-      //console.error(ll);
       var txt = tMsg.substring(1 + cmd.length);
+      const message = txt?.toString().toLowerCase().trim()
+
+      if (ignoreWords.includes(message)) {
+        console.log('txt is in the building', txt)
+        return;
+      }
+      if (message?.length <= 2) {
+        return;
+      }
 
       // Text must be at least 2 characters and max 200 characters
       var lazy = false;
@@ -128,8 +245,6 @@ function onMessageHandler(target, context, msg, self) {
           }
           else {
             // Translation
-            // TODO: Check is translated text == original text. In that case it
-            // means the command was not correctly used (ex: "!en hello friends")
             client.say(target, context['display-name'] + ' ' + ll[1] + ': ' + res.text);
           }
         }).catch(err => {
@@ -149,3 +264,6 @@ function onMessageHandler(target, context, msg, self) {
 function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
+
+// const ignoreLines = ['http', 'BikuBikuTest', '888', '８８８']
+// const deleteWords = ['saatanNooBow', 'BikuBikuTest'];
